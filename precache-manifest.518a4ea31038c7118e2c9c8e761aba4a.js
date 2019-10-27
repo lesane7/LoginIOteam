4400655ceffed206e1e8386fe27079989d5d60d2
CodeYourFuture <elkinalarca@gmail.com>
2019-10-27 14:29:38 -0500

    Updates

---
+++ precache-manifest.518a4ea31038c7118e2c9c8e761aba4a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9b339c78c82544517831562cd713e51e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "27d726bc2f449ebcfe37",
+    "url": "/static/css/main.7fc762d9.chunk.css"
+  },
+  {
+    "revision": "468e7296f39c20f89030",
+    "url": "/static/js/2.81c06ffa.chunk.js"
+  },
+  {
+    "revision": "27d726bc2f449ebcfe37",
+    "url": "/static/js/main.c387cf7d.chunk.js"
+  },
+  {
+    "revision": "40c747857fe0bc266df9",
+    "url": "/static/js/runtime-main.b5f0a657.js"
+  },
+  {
+    "revision": "0a23bef25b7079ed3d12ee4b77bd17cd",
+    "url": "/static/media/login.0a23bef2.jpg"
+  }
+]);
